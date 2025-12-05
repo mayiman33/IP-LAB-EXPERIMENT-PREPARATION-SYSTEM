90bd41bd9f05506a351a6d94cf780b6cda317c3c
Muhammad Afif Haziq <afifhziq24@gmail.com>
2025-12-05 10:19:23 +0800

    added code file
    added code file related booking and scheduling
    definitely need to change since the config differ

---
+++ Booking.java
@@ -0,0 +1,27 @@
+package com.labsystem.app.model;
+
+public class Booking {
+    private String subject;
+    private String timeSlot;
+    private String reservedBy;
+
+    // Constructor required to create dummy data in the Controller
+    public Booking(String subject, String timeSlot, String reservedBy) {
+        this.subject = subject;
+        this.timeSlot = timeSlot;
+        this.reservedBy = reservedBy;
+    }
+
+    // --- Getters (REQUIRED FOR THYMELEAF) ---
+    public String getSubject() {
+        return subject;
+    }
+
+    public String getTimeSlot() {
+        return timeSlot;
+    }
+
+    public String getReservedBy() {
+        return reservedBy;
+    }
+}
